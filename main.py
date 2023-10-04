aacaa65c737c1fb4a3ff993fc8371c5f9015e53e
Dawid Pruski(Prss161) <dpruski0@gmail.com>
2023-10-04 21:07:42 +0200

    Changing a code, also adding requirements via pipreqs

--- main.py
+++ main.py
@@ -1,44 +1,69 @@
 import csv
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+class TwitchVideoScraper:
+    def __init__(self, channel_url, max_links=20):
+        """
+        Args:
+            channel_url (str): url of a website with clips that we want to scrap.
+            max_links (int): amount of clips that we want. Defaults to 20.
+            
+        """
+        
+        self.channel_url = channel_url
+        self.max_links = max_links
+        self.link_list = []
+        self.title_list = []
+        self.channel_list = []
+
+    def scrape_videos(self):
+        driver = webdriver.Chrome()
+
+        try:
+            while len(self.link_list) < self.max_links:
+                page_url = f"{self.channel_url}"
+                driver.get(page_url)
+                time.sleep(random.uniform(2, 5))
+
+                links = driver.find_elements(By.XPATH, "//a[@data-a-target='preview-card-image-link']")
+                titles = driver.find_elements(By.XPATH, '//h3[@title]')
+                channels = driver.find_elements(By.XPATH, '//a[@data-a-target="preview-card-channel-link"]')
+
+                if not links:
+                    break
+
+                for link, title, channel in zip(links, titles, channels):
+                    self.link_list.append(link.get_attribute('href').strip())
+                    self.title_list.append(title.get_attribute('title').strip())
+                    self.channel_list.append(channel.get_attribute('href').strip())
+
+                if len(self.link_list) >= self.max_links:
+                    break
+
+        finally:
+            driver.quit()
+
+    def write_to_csv(self):
+        file_name = 'Links_for_clips.csv'
+        with open(file_name, 'w', encoding='utf-8', newline='') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(['Link', 'Title', 'Channel'])
+            for link, title, channel in zip(self.link_list, self.title_list, self.channel_list):
+                csv_writer.writerow([link, title, channel])
+
+    def run(self):
+        self.scrape_videos()
+        self.write_to_csv()
+        print(f"Scraped {len(self.link_list)} clips and saved to 'Links_for_clips'.")
 
 def main():
-    
-    file = open("./url.txt", "r")
-    list_of_channels = list(map(lambda x: x.strip("\n"), file.readlines()))
-    file.close()
-    driver = webdriver.Chrome()
-    
-    for channel in list_of_channels:
-        driver.get(channel)
-        time.sleep(3)
-        links = driver.find_elements(By.XPATH, "//a[@data-a-target='preview-card-image-link']")
-        tytul = driver.find_elements(By.XPATH, '//h3[@title]')
-        kanal = driver.find_elements(By.XPATH, '//a[@data-a-target="preview-card-channel-link"]')
-        linki = []
-        tytuly = []
-        kanaly = []
-        
-        for link in links:
-            string = link.get_attribute('href').strip()
-            linki.append(string)
+    channel_url = input("Enter the URL of the Twitch channel you want to scrape: ")
 
-        for title in tytul:
-            string = title.get_attribute('title').strip()
-            tytuly.append(string)
-            
-        for a in kanal:
-            string = a.get_attribute('href').strip()
-            kanaly.append(string)
-            
-        file_name = 'clips.csv'
-        with open(file_name, 'w', encoding='utf-8') as f:
-            f.write = csv.writer(f)
-            f.write.writerow(['Linki', 'Tytuly', 'Kanały'])
-            for i in range(len(linki)):
-                f.write.writerow([i + 1, tytuly[i], linki[i], kanaly[i]])
+    scraper = TwitchVideoScraper(channel_url)
+    scraper.run()
 
-
-main()
+if __name__ == "__main__":
+    main()
